8788d88c689b63385f152293ff68fe03bdb244ff
chuliming <chuliming@aiforward.com>
2022-09-01 09:57:07 +0800

    feat: husky

---
+++ .history/packages/smarty-ui-vite2/src/entry_20220901094051.ts
@@ -0,0 +1,14 @@
+import { App } from "vue"
+import { Button } from "./button/index.ts"
+import SFCButton from "./SFCButton.vue"
+import JSXButton from "./JSXButton"
+
+export { Button, SFCButton, JSXButton }
+
+export default {
+  install (app: App): void {
+    app.component(Button.name, Button)
+    app.component(SFCButton.name, SFCButton)
+    app.component(JSXButton.name, JSXButton)
+  },
+}
